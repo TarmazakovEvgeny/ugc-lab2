ae086bd7e820846db5bbf446aea5be7ae62ee172
Tarmazakov Evgeny <tarmazakov@mail.ru>
2015-12-02 16:07:35 +0300

    сделали построение диаграммы

--- src/main/java/ru/mephi/ugc/burndown/model/Task.java
+++ src/main/java/ru/mephi/ugc/burndown/model/Task.java
@@ -2,11 +2,13 @@ package ru.mephi.ugc.burndown.model;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.Comparator;
+import java.util.Date;
 
 @Entity
 @Table(name = "tasks")
 @NamedQueries({@NamedQuery(name = "Task.getAll", query = "SELECT t FROM Task t order by t.id")})
-public class Task implements Serializable {
+public class Task implements Serializable, Comparator<Task> {
 
     private static final long serialVersionUID = 1L;
     @Id
@@ -15,6 +17,8 @@ public class Task implements Serializable {
     private String name;
     private Integer complexity;
     private String status;
+    private Date startDate;
+    private Date finishDate;
 
     public Task(String item, Integer complexity, String status) {
         this.name = item;
@@ -55,6 +59,26 @@ public class Task implements Serializable {
 
     public void setComplexity(Integer complexity) {
         this.complexity = complexity;
+    }
+
+    public Date getStartDate() {
+        return startDate;
+    }
+
+    public void setStartDate(Date startDate) {
+        this.startDate = startDate;
+    }
+
+    public Date getFinishDate() {
+        return finishDate;
+    }
+
+    public void setFinishDate(Date finishDate) {
+        this.finishDate = finishDate;
+    }
+
+    public int compare(Task o1, Task o2) {
+        return o1.getFinishDate().compareTo(o2.getFinishDate());
     }
 }
 
